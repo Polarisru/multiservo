efc717d37feaeec79a3ef0da63e3358f5a552c92
Alex Kiselev <a.kiselev@volz-servos.com>
2021-04-30 16:29:51 +0200

    Meteor support started

--- FW/src/canbus_meteor.c
+++ FW/src/canbus_meteor.c
@@ -4,44 +4,18 @@
 static uint8_t tx_buff[CANBUS_MAX_LEN];
 static uint8_t rx_buff[CANBUS_MAX_LEN];
 
-/** \brief Send CAN packet and wait for reply if needed
- *
- * \param [in] tx_data Data buffer with packet to send
- * \param [in] tx_len Length of data packet to send
- * \param [out] rx_data Buffer with reply data
- * \param [in] timeout Timeout value in ms
- * \return True if reply was received
- *
- */
-bool CANBUS_TransferMeteor(uint8_t *tx_data, uint8_t tx_len, uint8_t *rx_data, uint8_t timeout)
+uint32_t CANBUS_BuildRequestID(uint8_t cmd)
 {
-  uint8_t errors = 0;
-  uint32_t ticks;
-  uint32_t id;
-  uint8_t data[CANBUS_MAX_LEN];
-  uint8_t cmd;
-  TCanMsg rx_msg;
+  CANBUS_IdType newID;
 
-  while (errors < CANBUS_MAX_RETRIES)
-  {
-    /**< First empty FIFO to avoid duplicated messages from last communication */
-    if (CANBUS_SendMessage(id, data, tx_len, true, false, false) == false)
-      return false;
+  newID.value = 0;
+  newID.bit.SRC = 0;
+  newID.bit.LCL = 1;
+  newID.bit.SP2 = 1;
+  newID.bit.SID = CANBUS_BROADCAST;
+  newID.bit.OC = cmd & ((1 << CANBUS_LEN_CMD) - 1);
 
-    /**< Wait till reply is received or timeout occurred */
-    ticks = xTaskGetTickCount() + timeout;
-    while (xTaskGetTickCount() < ticks)
-    {
-      if (CANBUS_ReceiveMessage(&rx_msg) == true)
-      {
-
-      }
-      vTaskDelay(1);
-    }
-    errors++;
-  }
-
-  return false;
+  return newID.value;
 }
 
 /** \brief Build ID for servo reply
@@ -61,6 +35,52 @@ uint32_t CANBUS_BuildReplyID(uint32_t id, uint8_t cmd)
   newID.bit.OC = cmd & ((1 << CANBUS_LEN_CMD) - 1);
 
   return newID.value;
+}
+
+/** \brief Send CAN packet and wait for reply if needed
+ *
+ * \param [in] tx_data Data buffer with packet to send
+ * \param [in] tx_len Length of data packet to send
+ * \param [out] rx_data Buffer with reply data
+ * \param [in] timeout Timeout value in ms
+ * \return True if reply was received
+ *
+ */
+bool CANBUS_TransferMeteor(uint32_t id, uint8_t *tx_data, uint8_t tx_len, uint8_t *rx_data, uint8_t timeout)
+{
+  uint8_t errors = 0;
+  uint32_t ticks;
+  CANBUS_IdType reply_id;
+  uint8_t cmd;
+  TCanMsg rx_msg;
+
+  reply_id.value = id;
+  cmd = reply_id.bit.OC + 1;
+  while (errors < CANBUS_MAX_RETRIES)
+  {
+    /**< First empty FIFO to avoid duplicated messages from last communication */
+    if (CANBUS_SendMessage(id, tx_data, tx_len, true, false, false) == false)
+      return false;
+
+    /**< Wait till reply is received or timeout occurred */
+    ticks = xTaskGetTickCount() + timeout;
+    while (xTaskGetTickCount() < ticks)
+    {
+      if (CANBUS_ReceiveMessage(&rx_msg) == true)
+      {
+        reply_id.value = rx_msg.id;
+        if (reply_id.bit.OC == cmd)
+        {
+          memcpy(rx_data, rx_msg.data, rx_msg.len);
+          return true;
+        }
+      }
+      vTaskDelay(1);
+    }
+    errors++;
+  }
+
+  return false;
 }
 
 /** \brief Build ID for servo reply for SETID command
@@ -90,6 +110,17 @@ uint32_t CANBUS_BuildReplyID(uint32_t id, uint8_t cmd)
  */
 bool CANBUS_SetPositionMeteor(float pos)
 {
+  int16_t i;
+  uint32_t id;
+
+  id = CANBUS_BuildRequestID(CAN_CMD_SETPOS);
+  i = (int16_t)(pos * 11.3778f);
+  tx_buff[0] = (uint8_t)(i >> 8);
+  tx_buff[1] = (uint8_t)i;
+
+  if (CANBUS_TransferMeteor(id, tx_buff, 2, rx_buff, CANBUS_TIMEOUT) == false)
+    return false;
+
   return true;
 }
 
@@ -114,10 +145,20 @@ bool CANBUS_GetPositionMeteor(float *pos)
  */
 bool CANBUS_ReadByteMeteor(uint16_t addr, uint8_t *value)
 {
+  uint32_t id;
+
   if (addr >= 0x100)
   {
     return false;
   }
+
+  id = CANBUS_BuildRequestID(CAN_CMD_READEEPROM);
+  tx_buff[0] = (uint8_t)addr;
+
+  if (CANBUS_TransferMeteor(id, tx_buff, 1, rx_buff, CANBUS_TIMEOUT) == false)
+    return false;
+
+  *value = rx_buff[1];
 
   return true;
 }
@@ -131,10 +172,19 @@ bool CANBUS_ReadByteMeteor(uint16_t addr, uint8_t *value)
  */
 bool CANBUS_WriteByteMeteor(uint16_t addr, uint8_t value)
 {
+  uint32_t id;
+
   if (addr >= 0x100)
   {
     return true;
   }
 
-  return true;
+  id = CANBUS_BuildRequestID(CAN_CMD_WRITEEEPROM);
+  tx_buff[0] = (uint8_t)addr;
+  tx_buff[1] = (uint8_t)value;
+
+  if (CANBUS_TransferMeteor(id, tx_buff, 2, rx_buff, CANBUS_TIMEOUT) == false)
+    return false;
+
+  return (rx_buff[1] == value);
 }
